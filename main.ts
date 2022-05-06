9af45e77ad0e89b3c0030c047ac52490fa8a8ecf
Diana Michelle Pérez R <82277294+ntmisel@users.noreply.github.com>
2022-05-06 16:23:44 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,14 @@
+basic.showString("Bienvenido")
+let b = randint(0, 10)
+let h = randint(0, 10)
+basic.showString("Base:")
+basic.showNumber(b)
+basic.pause(100)
+basic.showString("Altura:")
+basic.showNumber(h)
+basic.pause(100)
+let pre = b * h
+let area = pre / 2
+basic.showString("Area:")
+basic.showNumber(area)
+basic.pause(100)
